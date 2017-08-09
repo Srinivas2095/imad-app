a559ade83854663a08a04189d67b62bc7b787ea7
Srinivas2095 <30821799+Srinivas2095@users.noreply.github.com>
2017-08-09 13:20:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function createTemplate(data)
     ${title}
     </title>
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
-    <link href="ui/index.html" rel="stylesheet"/>
+    <link href="/ui/index.html" rel="stylesheet"/>
    <script type="text/javascript" src="/ui/main.js"></script>
     </head>
     <body>
