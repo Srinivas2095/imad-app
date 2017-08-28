086c15e28619a71de7548a1ce28b7b97711fe6fd
Srinivas2095 <30821799+Srinivas2095@users.noreply.github.com>
2017-08-28 11:46:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ function createTemplate(data)
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
