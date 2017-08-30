6a317e6bc50a2e885249feab19acdf6bc5c4f03e
Srinivas2095 <30821799+Srinivas2095@users.noreply.github.com>
2017-08-30 11:28:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,13 +61,13 @@ function createTemplate(data)
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-/*app.use(session({
+app.use(session({
     secret:'secretvalue',
-    cookie:{maxAge:1000:60*60*24:30},
+    cookie:{maxAge:1000*60*60*24*30}
     
     
 }));
-*/
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
