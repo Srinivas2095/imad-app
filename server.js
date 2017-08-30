7860ec273aaf5a017bcda1a64d4b81b93817c367
Srinivas2095 <30821799+Srinivas2095@users.noreply.github.com>
2017-08-30 11:19:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ var username=req.body.username;
                var hashedPassword=hash(password,salt);
                if(hashedPassword===dbString)
                {
-                   req.session.auth={userId:result.rows[0].id};
+                   req.session.auth={userId:result.rows[0].username};
                    res.send('credentials correct');
                }
                else
